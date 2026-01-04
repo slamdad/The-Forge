2548e61c448b78f5718aa264ec1c6613a4df0eb1
Slamdad <slamdad0000@gmail.com>
2026-01-04 22:53:05 +0530

    dvvs

---
+++ Python 101.py
@@ -0,0 +1,69 @@
+# #   # x = 1    # int
+# #   # y = 2.8  # float
+# #   # z = 1j   # complex
+
+# #   # #convert from int to float:
+# #   # a = float(x)
+
+# #   # #convert from float to int:
+# #   # b = int(y)
+
+# #   # #convert from int to complex:
+# #   # c = complex(x)
+
+# #   # print(a)
+# #   # print(b)
+# #   # print(c)
+
+# #   # print(type(a))
+# #   # print(type(b))
+# #   # print(type(c))
+
+# # # import random
+
+# # # print(random.randrange(1, 10))
+
+
+# # # import random
+# # # random.seed(42)
+# # # print([random.randrange(1,10) for _ in range(5)]
+
+# # print("It's alright")
+# # print("He is called 'Johnny'")
+# # print('He is called "Johnny"')
+
+# # a = """Lorem ipsum dolor sit amet,
+# # consectetur adipiscing elit,
+# # sed do eiusmod tempor incididunt
+# # ut labore et dolore magna aliqua."""
+# # print(a)
+
+# a = "Hello, World!"
+# print(a[0])
+
+# for x in "banana":
+#   print(x)
+
+# for d in "Slamdad":
+#   print(d)
+  
+# a = "Hello, World!"
+# print(len(a))
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+
+
+
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
+
+ 
+# txt = "The best things in life are free!"
+# print("expensive" not in txt)
+
+
+# txt = "The best things in life are free!"
+# if "expensive" not in txt:
+#   print("No, 'expensive' is NOT present.")
