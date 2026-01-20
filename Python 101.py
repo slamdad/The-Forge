bc550966126b8feb9606b3a7d5bb65ab71e75507
Slamdad <slamdad0000@gmail.com>
2026-01-20 12:36:00 +0530

    d

--- Python 101.py
+++ Python 101.py
@@ -67,3 +67,73 @@
 # txt = "The best things in life are free!"
 # if "expensive" not in txt:
 #   print("No, 'expensive' is NOT present.")
+
+# b = "Hello, World!"
+# print(b[2:5])
+
+# b = "Hello, World!"
+# print(b[-5:-2])
+
+# b = "Hello, World!"
+# print(b[-5:-2])
+
+# a = "Hello, World!"
+# print(a.upper())
+
+# a = "Hello, World!"
+# print(a.lower())
+
+# a = "   Hello, World! "
+# print(a.strip())
+
+# a = "Hello, World!"
+# print(a.replace("H", "J"))
+
+# a = "Hello, World!"
+# print(a.split(",")) 
+
+# a = "Hello"
+# b = "World"
+# c = a + b
+# print(c)
+
+# a = "Hello"
+# b = "World"
+# c = a + " " + b
+# print(c)
+
+
+# age = 36
+# #This will produce an error:
+# txt = f"My name is John, I am {age}"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price} dollars"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price:.2f} dollars"
+# print(txt)
+
+# txt = f"The price is {20 * 59} dollars"
+# print(txt)
+
+# txt = "We are the so-called \"Vikings\" from the north."
+# print(txt)
+
+# txt = "hello, and welcome to my world."
+# x = txt.capitalize()
+# print (x)
+
+# txt = "python is FUN!"
+# x = txt.capitalize()
+# print (x)
+
+# txt = "banana"
+# x = txt.center(20)
+# print(x)
+
+# txt = "My name is Ståle"
+# x = txt.encode()
+# print(x)
