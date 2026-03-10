bcdcdc74e14bd70e08c6be5683061d871222843f
slamdad <slamdad0000@gmail.com>
2026-03-10 12:51:08 +0530

    Add basic bill splitter and employee profile generator scripts

---
+++ Basic travel weather planner.py
@@ -0,0 +1,27 @@
+distance_mi = 200
+is_raining = True
+has_bike = True
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi:
+    print('False')
+
+elif distance_mi <= 1:
+    if not is_raining:
+        print('True')
+    else:
+        print('False')
+
+elif distance_mi <= 6:
+    if has_bike and not is_raining:
+        print('True')
+    else:
+        print("False")
+
+elif distance_mi > 6:
+    if has_car or has_ride_share_app:
+        print('True')
+    else:
+        print('False')
+
